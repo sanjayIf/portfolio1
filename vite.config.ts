5d750e10daff36ee421bed0be7d1e0a527c0c760
sanjayIf <157701633+sanjayIf@users.noreply.github.com>
2025-11-06 02:42:01 +0530

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio1/", 
+  base: "/", 
 
   server: {
     host: "::",
